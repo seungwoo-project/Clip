2c9850517de1bbfbedda9a3e5cd918b6c0aa8827
leeseungwoo <112483124+seungwoo-project@users.noreply.github.com>
2024-05-28 18:25:40 +0900

    프로젝트 생성

---
+++ AI-InterviewService/src/main/java/devdays/AI_InterviewService/AiInterviewServiceApplication.java
@@ -0,0 +1,13 @@
+package devdays.AI_InterviewService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiInterviewServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiInterviewServiceApplication.class, args);
+	}
+
+}
---
+++ AI-InterviewService/src/test/java/devdays/AI_InterviewService/AiInterviewServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package devdays.AI_InterviewService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiInterviewServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
