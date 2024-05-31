bfb77fcce15209acab28737acfb3c31b55fdc05d
이승우 <oltmddn123@naver.com>
2024-05-31 22:44:39 +0900

    컨트롤러 구현

--- AI-InterviewService/src/main/java/devdays/AI_InterviewService/controller/LoginController.java
+++ AI-InterviewService/src/main/java/devdays/AI_InterviewService/controller/LoginController.java
@@ -2,15 +2,28 @@ package devdays.AI_InterviewService.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 //@ResponseBody
-public class InterviewController {
+public class LoginController {
 
     @GetMapping("/")
     public String 로그인폼() {
 
-        return "ok";
+        return "basic/login";
+    }
+
+    @PostMapping("/")
+    public String login() {
+
+        return "basic/list";
+    }
+
+    @GetMapping("/register")
+    public String register() {
+
+        return "basic/register";
     }
 }
