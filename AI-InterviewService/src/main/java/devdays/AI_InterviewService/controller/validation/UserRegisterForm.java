8b08dfe7939c0cc1ed18f39a5e734d596a44fc8e
leeseungwoo <oltmddn123@naver.com>
2024-07-04 15:25:05 +0900

    refactor: 비밀번호 유효성검증 리팩토링

--- AI-InterviewService/src/main/java/devdays/AI_InterviewService/controller/validation/UserRegisterForm.java
+++ AI-InterviewService/src/main/java/devdays/AI_InterviewService/controller/validation/UserRegisterForm.java
@@ -13,7 +13,10 @@ public class UserRegisterForm {
     @Size(min = 4, message = "아이디는 최소 4자 이상이어야 합니다.")
     private String userId;
 
-    @Pattern(regexp = "^[a-zA-Z0-9]*$", message = "비밀번호는 영어와 숫자로 입력해 주시기 바랍니다.")
+    @Pattern(
+            regexp = "^(?=.*[A-Za-z])(?=.*\\d)[A-Za-z\\d!@#$%^&*()_+\\-=\\[\\]{};':\"\\\\|,.<>\\/?]*$",
+            message = "비밀번호는 영어와 숫자를 포함해야 하며, 특수문자는 선택적으로 사용할 수 있습니다."
+    )
     @Size(min = 4, message = "비밀번호는 최소 4자 이상이어야 합니다.")
     private String password;
 
